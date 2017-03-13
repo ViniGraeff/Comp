025ba70a81e5a3c75b654836f54b9ca4d5bdcacb
Vinícius Graeff Sturmer <vinicius.sturmer@digitaldesk.com.br>
2017-03-13 16:55:42 -0300

    nada funcionando

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	// Apenas modifique de ejs para jade.
+	app.set('view engine', 'jade');
+	app.set('view options', {layout: false});
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+});
+
 router.get('/', function(req, res){
-	res.json({'msg': 'Seja bem-vindo!'});
+	res.send('<h1>Seja bem-vindo!</h1>');
 });
 
 module.exports = router;

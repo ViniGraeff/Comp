f0d61df6b00a30e52c236ebee622647820a4a10a
Vinícius Graeff Sturmer <vinicius.sturmer@digitaldesk.com.br>
2017-03-13 14:32:36 -0300

    server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var port = '12000'
+
+var application = require('./app');
+app.use(application);
+
+app.listen(port);
+console.log('Servidor iniciado na porta '+port+'. Acesse: localhost:'+port)
